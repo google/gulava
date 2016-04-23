f98fbd0e56457eda753ad276786c78bb7508ab26
Matthew DeVore <matvore@google.com>
2016-04-23 07:49:34 -0700

    CachedGoal v1
    I plan to use this for implementing a DisjGoal->ConjGoal converter. To do that,
    you have to turn:
    
    (A & B) | C
    
    into:
    
    (A | C) & (B | C)
    
    To make the latter work equivalently, (1) C cannot evaluate differently the two
    times it is run, and (2) B should not be run if A fails. This first commit
    handles (1).
    
    For (1), if C can succeed in multiple ways, it should succeed in the same way in
    (A | C) as it does in (B | C), or else extra substitutions will be generated
    which combine the correct substitutions.
    
    For (2), if B is evaluated when A fails, then (B | C) will succeed more times
    than it ought to, and there will be junk substitutions in the stream where B
    pointlessly adds bindings. To make this work, CachedGoal will also support
    prerequisite CachedGoals, which means the delegate Goal will not be attempted
    if some other CachedGoal is not already solved.
    
    Additionally, though not a requirement for correctness, (3) we should aim for
    that in (A | C) & (B | C), if C fails in (A | C), it is not re-evaluated in
    (B | C), in case C is very expensive. This can be accomplished by replacing
    (B | C) with (B | C'), where C' is Goals.UNIT with C as a prerequisite.

---
+++ java/gulava/CachedGoal.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (c) 2016 The Gulava Authors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+package gulava;
+
+import java.io.IOException;
+
+/**
+ * A goal which will not be run more than once after it has already passed. This is not just an
+ * optimization - it can also change the result stream. If a {@code CachedGoal} wraps a
+ * {@link DisjGoal} and the {@code CachedGoal} appears multiple times in some larger composite goal,
+ * then the {@link DisjGoal} is guaranteed to only {@link Goal#run(Subst)} once, and so will choose
+ * the same alternate goal each time it appears. See the test
+ * {@link CachedGoalTest#disjIsNotReevaluated()} for an example of this.
+ */
+public final class CachedGoal implements Dumpable, Goal {
+  private final Goal delegate;
+
+  public CachedGoal(Goal delegate) {
+    this.delegate = delegate;
+  }
+
+  @Override
+  public Stream run(Subst s) {
+    Object cached = s.get(this);
+    if (cached == null) {
+      return delegate.run(s.ext(this, true));
+    } else {
+      return new SolveStep(s, EmptyStream.INSTANCE);
+    }
+  }
+
+  @Override
+  public void dump(Dumper dumper) throws IOException {
+    dumper.dump(
+        "CachedGoal@" + Integer.toString(System.identityHashCode(this), 36),
+        delegate);
+  }
+}
---
+++ javatests/gulava/CachedGoalTest.java
@@ -0,0 +1,101 @@
+/*
+ *  Copyright (c) 2016 The Gulava Authors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+package gulava;
+
+import static gulava.Goals.conj;
+import static gulava.Goals.disj;
+import static gulava.Goals.same;
+
+import gulava.testing.AssertingWriter;
+import gulava.testing.LogicAsserter;
+import gulava.testing.RecordsCallGoal;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CachedGoalTest {
+  static final Var X = new Var();
+  static final Var Y = new Var();
+
+  @Test
+  public void disjIsNotReevaluated() {
+    Goal cached = new CachedGoal(disj(same(X, 42), same(Y, 24)));
+
+    new LogicAsserter()
+        .stream(conj(cached, cached))
+        .addRequestedVar(X, Y)
+        .startSubst()
+        .put(X, 42)
+        .startSubst()
+        .put(Y, 24)
+        .test();
+  }
+
+  @Test
+  public void multipleCachedGoals() {
+    Goal g1 = new CachedGoal(same(X, 44));
+    Goal g2 = new CachedGoal(same(Y, 99));
+
+    new LogicAsserter()
+        .stream(conj(g1, g2))
+        .addRequestedVar(X, Y)
+        .startSubst()
+        .put(X, 44)
+        .put(Y, 99)
+        .test();
+  }
+
+  @Test
+  public void runNotInvokedAgain() {
+    StringBuilder callReport = new StringBuilder();
+    RecordsCallGoal callOnce = new RecordsCallGoal(callReport, "a");
+    CachedGoal goal = new CachedGoal(callOnce);
+
+    new LogicAsserter()
+        .stream(conj(goal, goal))
+        .startSubst()
+        .test();
+
+    Assert.assertEquals("a", callReport.toString());
+  }
+
+  @Test
+  public void dumpIncludesIdentityHashCode() throws Exception {
+    Goal subGoal = same(X, 42);
+    Goal goal = new CachedGoal(subGoal);
+
+    AssertingWriter writer = new AssertingWriter();
+    new Dumper(/*indentation=*/0, writer)
+        .dump(goal);
+
+    // Each CachedGoal object is unequal with other instances, since it only caches itself, not the
+    // delegate goal. So we don't want two CachedGoals with the same delegate Goal to appear the
+    // same in dumps. identityHashCode does not guarantee unique return values for every Object, but
+    // it should be good enough in practice.
+    writer.assertLines(
+        "CachedGoal@" + Integer.toString(System.identityHashCode(goal), 36),
+        "  " + subGoal);
+  }
+}
